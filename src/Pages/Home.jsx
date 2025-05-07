1faedf30815d637a77b0dd8c66d30fcb190aba8d
shvgpt-070208 <shvgpt070208@gla.ac.in>
2025-05-07 21:04:06 +0530

    final update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,11 +8,11 @@ const Home = () => {
     Greet()
   },[])
   return (
-    <div className='w-screen flex  min-h-screen max-[440px]:flex-col-reverse pt-10    mb-10 pl-2'>
+    <div className='w-screen flex  min-h-screen  pt-10    mb-10 pl-2'>
      <div className="intro w-1/2 min-[440px]:mt-30 min-w-[300px] m-auto">
-      <h1 className='block m-auto text-sm text-white'>Hi,</h1>
-      <h1 className='introtext'>  I'm Shivam Gupta</h1>
-      <p className='w-2/3 leading-tighter text-white text-justify text-[0.8rem] min-w-[270px]'>A Developer with a passion for creating innovative digital solutions. With a keen eye for detail and a love for learning, I craft unique experiences that blend creativity and functionality. Explore my work to see how I bring ideas to life through design, development, and collaboration.</p>
+      
+      <h1 className='text-white text[2rem]'>Hi,<span className='introtext'> I'm Shivam Gupta</span></h1>
+      <p className='w-2/3 leading-tighter text-white text-left text-[0.8rem] min-w-[270px]'>A Developer with a passion for creating innovative digital solutions. With a keen eye for detail and a love for learning, I craft unique experiences that blend creativity and functionality. Explore my work to see how I bring ideas to life through design, development, and collaboration.</p>
       <button className='mr-4 mt-6 bg-blue-600 px-2 rounded text-white py-1 cursor-pointer'>Explore!</button>
       <button className='mt-6 bg-green-600 px-2 rounded text-white py-1 cursor-pointer'>Hire Me</button>
      </div>
